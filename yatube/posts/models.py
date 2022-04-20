025d2ed4d8c133fa5ff9a3924e72f0283c7bd700
Ильшат Кушманбетов <ilshat261093@yandex.ru>
2022-04-20 22:34:57 +0300

    Спринт 5 Blog
    * 1.1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django import forms
 
-from pytils.translit import slugify
-
 User = get_user_model()
 
 
@@ -51,11 +49,6 @@ class Group(models.Model):
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)[:200]
-        super().save(*args, **kwargs)
 
 
 class PostForm(forms.ModelForm):
